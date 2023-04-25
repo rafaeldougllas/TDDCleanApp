3ac4bc705975ded0a8e8f2d3dc08296c74e93487
Rafael Douglas <rafael.dougllas@gmail.com>
2023-04-25 11:49:51 -0300

    Creating Infra Layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  AlamofireAdapterTests.swift
+//  InfraTests
+//
+//  Created by Rafael Douglas Sousa Barreto Dos Santos on 25/04/23.
+//
+
+import XCTest
+
+final class AlamofireAdapterTests: XCTestCase {
+    func test_() {
+        
+    }
+}
